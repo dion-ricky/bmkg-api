09d6b2bdd4cdcc20816c3a3ad4c205fdcdba025c
Dion Ricky <dionrickysptr@gmail.com>
2018-04-14 09:56:47 +0700

    fetch more data #1

--- api/system/date_system.php
+++ api/system/date_system.php
@@ -51,5 +51,47 @@ function expandDate($str){
   return ($str[1]." ".$bulan." ".date("Y"));
 }
 
-
+function normalDate($str){
+  preg_match("/[\w,]+\s*(\d+)\s*(\w+)/", $str, $regstr);
+  switch($regstr[2]) {
+    case "Jan":
+      $bulan = "1";
+      break;
+    case "Feb":
+      $bulan = "2";
+      break;
+    case "Mar":
+      $bulan = "3";
+      break;
+    case "Apr":
+      $bulan = "4";
+      break;
+    case "Mei":
+      $bulan = "5";
+      break;
+    case "Jun":
+      $bulan = "6";
+      break;
+    case "Jul":
+      $bulan = "7";
+      break;
+    case "Agu":
+    case "Ags":
+      $bulan = "8";
+      break;
+    case "Sep":
+      $bulan = "9";
+      break;
+    case "Okt":
+      $bulan = "10";
+      break;
+    case "Nov":
+      $bulan = "11";
+      break;
+    case "Des":
+      $bulan = "12";
+      break;
+  }
+  return $regstr[1].'-'.$bulan.'-'.date("Y");
+}
 ?>
