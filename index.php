61ae179ca4fb427d7bd030cdde597a7a6bb23290
Dion Ricky <dionrickysptr@gmail.com>
2018-05-16 08:45:47 +0700

    UI First Commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="favicon.ico">
+
+  <title>BMKG API by Ignite Project</title>
+
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/font-awesome.css" rel="stylesheet">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <a class="navbar-brand" href="#">
+      <img alt="logo" src="branding_icon.ico" height="30.4" width="30.4">&nbsp; BMKG Weather API
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="#"><span class="fa fa-home"></span>&nbsp; Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="#"><span class="fa fa-lightbulb-o"></span>&nbsp; Examples</a>
+        <a class="nav-item nav-link" href="#"><span class="fa fa-file-text"></span>&nbsp; Documentation</a>
+        <a class="nav-item nav-link" href="#"><span class="fa fa-search"></span>&nbsp; Search</a>
+        <a class="nav-item nav-link" href="#"><span class="fa fa-balance-scale"></span>&nbsp; License</a>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container-fluid">
+    <div class="row">
+
+      <main role="main" class="col-md-9 col-lg-10">
+
+      </main>
+
+    </div>
+  </div>
+
+  <script src="js/vendor/jquery-slim.min.js"></script>
+  <script src="js/vendor/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
+<?php
+?>
