33b5e1401b8db4fdfda795d8ad2f82fd2ec0f918
Dion Ricky <dionrickysptr@gmail.com>
2018-05-17 17:13:25 +0700

    create folder docs, exaample, search

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
 <!doctype html>
-<!--
-
-   Copyright 2018 Ignite Project
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-
--->
 <html lang="en">
 <head>
   <meta charset="utf-8">
@@ -31,20 +14,21 @@
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+  <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light shadow-sm">
+    <img alt="logo" src="branding_icon.ico" height="30.4" width="30.4">
     <a class="navbar-brand" href="#">
-      <img alt="logo" src="branding_icon.ico" height="30.4" width="30.4">&nbsp; BMKG Weather API
+      &nbsp; BMKG Weather API
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
       <div class="navbar-nav">
-        <a class="nav-item nav-link active" href="#"><span class="fa fa-home"></span>&nbsp; Home <span class="sr-only">(current)</span></a>
-        <a class="nav-item nav-link" href="#"><span class="fa fa-lightbulb-o"></span>&nbsp; Examples</a>
-        <a class="nav-item nav-link" href="#"><span class="fa fa-file-text"></span>&nbsp; Documentation</a>
-        <a class="nav-item nav-link" href="#"><span class="fa fa-search"></span>&nbsp; Search</a>
-        <a class="nav-item nav-link" href="#"><span class="fa fa-balance-scale"></span>&nbsp; License</a>
+        <a class="nav-item nav-link active" href="./"><span class="fa fa-home"></span>&nbsp; Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="./example"><span class="fa fa-lightbulb-o"></span>&nbsp; Examples</a>
+        <a class="nav-item nav-link" href="./docs"><span class="fa fa-file-text"></span>&nbsp; Documentation</a>
+        <a class="nav-item nav-link" href="./search"><span class="fa fa-search"></span>&nbsp; Search</a>
+        <a class="nav-item nav-link" href="./license"><span class="fa fa-balance-scale"></span>&nbsp; License</a>
       </div>
     </div>
   </nav>
