6a71c499ca2457ac468568e4a9244a83069db951
Dion Ricky <dionrickysptr@gmail.com>
2018-05-18 08:20:25 +0700

    ui update

---
+++ example/index.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="favicon.ico">
+
+  <title>BMKG API by Ignite Project</title>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <link href="../css/bootstrap.min.css" rel="stylesheet">
+  <link href="../css/font-awesome.css" rel="stylesheet">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light shadow-sm">
+    <img alt="logo" src="../branding_icon.ico" height="30.4" width="30.4">
+    <a class="navbar-brand" href="#">
+      &nbsp; BMKG Weather API
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link" href="../"><span class="fa fa-home"></span>&nbsp; Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link active" href="./"><span class="fa fa-lightbulb-o"></span>&nbsp; Examples</a>
+        <a class="nav-item nav-link" href="../docs"><span class="fa fa-file-text"></span>&nbsp; Documentation</a>
+        <a class="nav-item nav-link" href="../search"><span class="fa fa-search"></span>&nbsp; Search</a>
+        <a class="nav-item nav-link" href="../osl"><span class="fa fa-balance-scale"></span>&nbsp; License</a>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container-fluid">
+    <div class="row">
+
+      <main role="main" class="col" style="margin-top: 4rem">
+        <div class="row">
+          <div class="col mt-3">
+            <div class="card">
+              <div class="card-header">Pagi</div>
+              <div class="card-body">
+                  <h5 class="card-title">Cerah</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+          <div class="col mt-3">
+            <div class="card">
+              <div class="card-header">Siang</div>
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+          <div class="col mt-3">
+            <div class="card">
+              <div class="card-header">Malam</div>
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+          <div class="col mt-3">
+            <div class="card">
+              <div class="card-header">Dini Hari</div>
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+
+    </div>
+  </div>
+  
+  <script src="../js/vendor/popper.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+
+</body>
+</html>
+<?php
+?>
